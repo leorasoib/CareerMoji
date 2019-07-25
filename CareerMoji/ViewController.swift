17e80571088c32a15b252a016c72f2eb2e47dbd9
Apple <apple@Laptop-1-01.local>
2019-07-25 15:35:33 -0400

    Iteration 1

--- CareerMoji/ViewController.swift
+++ CareerMoji/ViewController.swift
@@ -14,7 +14,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+   
+        
+     @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Hello there!", message: "You got this", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
